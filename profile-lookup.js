56a5410c251d4b3244a60f2d2830db6e0a10bfe6
David Dickinson <rnddave@gmail.com>
2022-03-11 11:36:32 +0000

    profile-lookup.js not working, wrong path I think

--- profile-lookup.js
+++ profile-lookup.js
@@ -18,79 +18,95 @@ If prop does not correspond to any valid properties of a contact found to match 
 // Setup
 const contacts = [
     {
-      firstName: "Akira",
-      lastName: "Laine",
-      number: "0543236543",
-      likes: ["Pizza", "Coding", "Brownie Points"],
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
     },
     {
-      firstName: "Harry",
-      lastName: "Potter",
-      number: "0994372684",
-      likes: ["Hogwarts", "Magic", "Hagrid"],
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
     },
     {
-      firstName: "Sherlock",
-      lastName: "Holmes",
-      number: "0487345643",
-      likes: ["Intriguing Cases", "Violin"],
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
     },
     {
-      firstName: "Kristian",
-      lastName: "Vos",
-      number: "unknown",
-      likes: ["JavaScript", "Gaming", "Foxes"],
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
     },
-  ];
-  
-  function lookUpProfile(name, prop) {
-    // Only change code below this line
-  
-
-    
-
-    // Only change code above this line
-  }
-  
-  lookUpProfile("Akira", "likes");
-
-
-  /*
-
-  STARTING CODE >>>> 
-
-// Setup
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
 ];
 
 function lookUpProfile(name, prop) {
-  // Only change code below this line
+    // Only change code below this line
 
-  // Only change code above this line
+    // if the [name] is a firstname then {if-a}
+    // if the [prop] exists for that [name] (nested if statements I'm thinking) {if-b}
+    // if both {if-a && if-b} true then [return] the [value] of that property
+    if (name == contacts.firstName && prop == contacts.likes) {
+        return contacts.likes[prop];
+    }
+    else if (name != contacts.firstName) {
+        return "No such contact"
+    }
+    else if (name == contacts.firstName && prop != contacts.likes) {
+        return "No such property"
+    }
+
+
+    // if [name] does not correspond to any contacts then return the string [No such contact]
+
+    // if [prop] no correspond to any valid properties, then return [No such property]
+
+
+    // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
+
+
+/*
+
+STARTING CODE >>>> 
+
+// Setup
+const contacts = [
+{
+  firstName: "Akira",
+  lastName: "Laine",
+  number: "0543236543",
+  likes: ["Pizza", "Coding", "Brownie Points"],
+},
+{
+  firstName: "Harry",
+  lastName: "Potter",
+  number: "0994372684",
+  likes: ["Hogwarts", "Magic", "Hagrid"],
+},
+{
+  firstName: "Sherlock",
+  lastName: "Holmes",
+  number: "0487345643",
+  likes: ["Intriguing Cases", "Violin"],
+},
+{
+  firstName: "Kristian",
+  lastName: "Vos",
+  number: "unknown",
+  likes: ["JavaScript", "Gaming", "Foxes"],
+},
+];
+
+function lookUpProfile(name, prop) {
+// Only change code below this line
+
+// Only change code above this line
 }
 
 lookUpProfile("Akira", "likes");
