ed52463eb59a707677944515201293e88e51c8df
David Dickinson <rnddave@gmail.com>
2022-03-15 14:15:41 +0000

    ecmaScript6.js - 52% complete

--- ecmaScript6.js
+++ ecmaScript6.js
@@ -190,11 +190,89 @@ const HIGH_TEMPERATURES = {
 const {today: highToday} = HIGH_TEMPERATURES;
 const {tomorrow: highTomorrow} = HIGH_TEMPERATURES;
 
+/*
+One key difference between the spread operator and array destructuring is that the spread operator unpacks all contents of an array into a comma-separated list. Consequently, you cannot pick or choose which elements you want to assign to variables.
+
+const [a, b] = [1, 2, 3, 4, 5, 6];
+console.log(a, b);
+
+The console will display the values of a and b as 1, 2.
+*/
+
+// We can also access the value at any index in an array with destructuring by using commas to reach the desired index
+
+const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
+console.log(a, b, c);                       // 1 2 5
 
 
+const [a40, b40, ...arr40] = [1, 2, 3, 4, 5, 7];
+console.log(a40, b40);
+console.log(arr40);
+/*
+1 2
+[ 3, 4, 5, 7 ]
+*/
 
+/*
+A new feature of ES6 is the template literal. This is a special type of string that makes creating complex strings easier.
 
+Template literals allow you to create multi-line strings and to use string interpolation features to create strings.
+*/
 
+const person = {
+    name: "Zodiac Hasbro",
+    age: 56
+  };
+  
+  const greeting2 = `Hello, my name is ${person.name}!
+  I am ${person.age} years old.`;
+  
+  console.log(greeting2);
+
+  /*
+  Hello, my name is Zodiac Hasbro!
+  I am 56 years old.
+  */
+
+ /*
+ Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and have a class attribute with the value text-warning. The makeList function should return the array of list item strings.
+
+Use an iterator method (any kind of loop) to get the desired output (shown below).
+
+[
+  '<li class="text-warning">no-var</li>',
+  '<li class="text-warning">var-on-top</li>',
+  '<li class="text-warning">linebreak</li>'
+]
+*/
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+    }
+    // Only change code above this line
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
+
+  console.log(failuresList);
+
+/* 
+[
+  '<li class="text-warning">no-var</li>',
+  '<li class="text-warning">var-on-top</li>',
+  '<li class="text-warning">linebreak</li>'
+] 
+*/
 
 
 
