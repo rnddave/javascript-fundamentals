b26db7ea450694e447dc0512d16352ca71058093
David Dickinson <rnddave@gmail.com>
2022-03-15 14:33:10 +0000

    ecmaScript6.js - 62% complete

--- ecmaScript6.js
+++ ecmaScript6.js
@@ -275,5 +275,46 @@ const result = {
 */
 
 
+/*
+ES6 provides a new syntax to create objects, using the class keyword.
+
+It should be noted that the class syntax is just syntax, and not a full-fledged class-based implementation of an object-oriented paradigm, unlike in languages such as Java, Python, Ruby, etc.
+
+n ES5, we usually define a constructor function and use the new keyword to instantiate an object.
+
+var SpaceShuttle = function(targetPlanet){
+  this.targetPlanet = targetPlanet;
+}
+var zeus = new SpaceShuttle('Jupiter');
+The class syntax simply replaces the constructor function creation:
+
+class SpaceShuttle {
+  constructor(targetPlanet) {
+    this.targetPlanet = targetPlanet;
+  }
+}
+const zeus = new SpaceShuttle('Jupiter');
+It should be noted that the class keyword declares a new function, to which a constructor is added. This constructor is invoked when new is called to create a new object.
+
+Note: UpperCamelCase should be used by convention for ES6 class names
+*/
+
+class Vegetable {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+
+  
+  const carrot = new Vegetable('carrot');
+  console.log(carrot.name); // Should display 'carrot'
+
+  
+
+
+
+
+
+
 
 
