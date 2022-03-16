a8dbfb6aac27885ec816cfff8b1849e465218307
David Dickinson <rnddave@gmail.com>
2022-03-16 10:07:51 +0000

    ecmaScript6.js - 79% complete

--- ecmaScript6.js
+++ ecmaScript6.js
@@ -422,3 +422,17 @@ export const uppercaseString = (string) => {
 
 // NB I commented out export as failed in test script. 
 
+/*
+Suppose you have a file and you wish to import all of its contents into the current file. This can be done with the import * as syntax. Here's an example where the contents of a file named math_functions.js are imported into a file in the same directory:
+
+import * as myMathModule from "./math_functions.js";
+
+example >>> 
+
+import * as stringFunctions from './string_functions.js'
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+*/
+
