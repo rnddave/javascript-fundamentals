5092255ce100c4fece15d473d61873420fa7db5b
David Dickinson <rnddave@gmail.com>
2022-03-22 10:35:26 +0000

    regex.js 94% complete

--- regex.js
+++ regex.js
@@ -452,3 +452,33 @@ let myRegex91 = /(Franklin|Eleanor)( |.* )Roosevelt\b/g; // Change this line
 let result91 = myRegex91.test(myString91);
 
 
+// Say you want to match a word that occurs multiple times like below.
+
+let repeatStr94 = "row row row your boat";
+
+// You could use /row row row/, but what if you don't know the specific word repeated? Capture groups can be used to find repeated substrings.
+
+/*
+Capture groups are constructed by enclosing the regex pattern to be captured in parentheses. In this case, the goal is to capture a word consisting of alphanumeric characters so the capture group will be \w+ enclosed by parentheses: /(\w+)/.
+
+The substring matched by the group is saved to a temporary "variable", which can be accessed within the same regex using a backslash and the number of the capture group (e.g. \1). Capture groups are automatically numbered by the position of their opening parentheses (left to right), starting at 1.
+
+The example below matches a word that occurs thrice separated by spaces: */
+
+let repeatRegex94 = /(\w+) \1 \1/;
+repeatRegex94.test(repeatStr94); // Returns true
+repeatStr94.match(repeatRegex94); // Returns ["row row row", "row"]
+console.log(repeatStr94.match(repeatRegex94));
+
+// Using the .match() method on a string will return an array with the matched substring, along with its captured groups.
+
+/*
+[
+  'row row row',
+  'row',
+  index: 0,
+  input: 'row row row your boat',
+  groups: undefined
+]
+*/
+
