f6916c2d0febed7185548e5b6dfa7e8dc3f35868
David Dickinson <rnddave@gmail.com>
2022-03-23 15:03:01 +0000

    data-structures.js = 40% complete

--- data-structures.js
+++ data-structures.js
@@ -155,9 +155,60 @@ function htmlColorNames(arr) {
   
   console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
 
+/*
+The next method we will cover is slice(). Rather than modifying an array, slice() copies or extracts a given number of elements to a new array, leaving the array it is called upon untouched. slice() takes only 2 parameters — the first is the index at which to begin extraction, and the second is the index at which to stop extraction (extraction will occur up to, but not including the element at this index). Consider this:
+
+let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear'];
+
+let todaysWeather = weatherConditions.slice(1, 3);
+todaysWeather would have the value ['snow', 'sleet'], while weatherConditions would still have ['rain', 'snow', 'sleet', 'hail', 'clear'].
+
+In effect, we have created a new array by extracting elements from an existing array.
+*/
+
+// We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the string elements warm and sunny.
+
+function forecast(arr) {
+    // Only change code below this line
+    let newArr = arr.slice(2, 4)
+
+    return newArr;
   
+  }
+    
+  // Only change code above this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
 
 
+  /*
+  ES6's new spread operator allows us to easily copy all of an array's elements, in order, with a simple and highly readable syntax. The spread syntax simply looks like this: ...
+
+In practice, we can use the spread operator to copy an array like so:
+
+let thisArray = [true, true, undefined, false, null];
+let thatArray = [...thisArray];
+
+thatArray equals [true, true, undefined, false, null]. thisArray remains unchanged and thatArray contains the same elements as thisArray.
+*/
+
+
+// We have defined a function, copyMachine which takes arr (an array) and num (a number) as arguments. The function is supposed to return a new array made up of num copies of arr. We have done most of the work for you, but it doesn't work quite right yet. Modify the function using spread syntax so that it works correctly (hint: another method we have already covered might come in handy here!).
+
+
+function copyMachine(arr, num) {
+    let newArr_40 = [];
+    while (num >= 1) {
+      // Only change code below this line
+      newArr_40.push([...arr]);
+      // Only change code above this line
+      num--;
+    }
+    return newArr_40;
+  }
+  
+  console.log(copyMachine([true, false, true], 2));     // [ [ true, false, true ], [ true, false, true ] ]
+
+  
 
 
 
