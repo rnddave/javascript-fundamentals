2958b5965e8a1745d11d5cc2fa94b0a48dc908ad
David Dickinson <rnddave@gmail.com>
2022-03-23 10:47:43 +0000

    debug.js = 25% complete

--- debug.js
+++ debug.js
@@ -8,3 +8,18 @@ runtime errors where your code has unexpected behavior,
 logical errors where your code doesn't do what you intended.
 
 */
+
+console.log(typeof "");
+console.log(typeof 0);
+console.log(typeof []);
+console.log(typeof {});
+
+/*
+string
+number
+object
+object
+*/
+
+// JavaScript recognizes seven primitive (immutable) data types: Boolean, Null, Undefined, Number, String, Symbol (new with ES6), and BigInt (new with ES2020), and one type for mutable items: Object. Note that in JavaScript, arrays are technically a type of object.
+
