5bea4e48bd11d42358d9cf5f17161304a87a1171
David Dickinson <rnddave@gmail.com>
2022-03-24 13:14:08 +0000

    data-structures.js = 100% complete

--- data-structures.js
+++ data-structures.js
@@ -402,6 +402,66 @@ function countOnline(usersObj) {
 console.log(countOnline(users85));          // 1
 
 
+// Finish writing the getArrayOfUsers function so that it returns an array containing all the properties in the object it receives as an argument.
+
+let users95 = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function getArrayOfUsers(obj) {
+    // Only change code below this line
+
+    return Object.keys(obj);
+    // Only change code above this line
+  }
+  
+  console.log(getArrayOfUsers(users95));        // [ 'Alan', 'Jeff', 'Sarah', 'Ryan' ]
+
+  // Take a look at the object we've provided in the code editor. The user object contains three keys. The data key contains five keys, one of which contains an array of friends. From this, you can see how flexible objects are as data structures. We've started writing a function addFriend. Finish writing it so that it takes a user object and adds the name of the friend argument to the array stored in user.data.friends and returns that array.
+
+  let user99 = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+      username: 'kennethCodesAllDay',
+      joinDate: 'March 26, 2016',
+      organization: 'freeCodeCamp',
+      friends: [
+        'Sam',
+        'Kira',
+        'Tomo'
+      ],
+      location: {
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'USA'
+      }
+    }
+  };
+  
+  function addFriend(userObj, friend) {
+    // Only change code below this line
+    userObj.data.friends.push(friend);
+    return userObj.data.friends;
+    // Only change code above this line
+  }
+  
+  console.log(addFriend(user99, 'Pete'));       // [ 'Sam', 'Kira', 'Tomo', 'Pete' ]
 
 
 
