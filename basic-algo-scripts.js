4ec6d6a64ba4dd0673fac976ea2f5f79d0528ddc
David Dickinson <36966460+rnddave@users.noreply.github.com>
2022-03-30 14:48:12 +0100

    basic-algo-scripts.js = 38% complete

--- basic-algo-scripts.js
+++ basic-algo-scripts.js
@@ -139,6 +139,20 @@ largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -1
 [ 25, 48, 21, -3 ]
 */
 
+/*
+Confirm the Ending
+Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+*/
+
+function confirmEnding(str, target) {
+    console.log(str.indexOf(target, str.length - target.length) !== -1);    //true
+    return str.indexOf(target, str.length - target.length) !== -1;
+  }
+  
+  confirmEnding("Bastian", "n");
+
 
 
 
