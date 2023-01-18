baba34488e1ce7a56df3a832ae73dc1eaea446cc
David Dickinson <rnddave@gmail.com>
2023-01-18 11:43:17 +0000

    just adding some console log messages to review for test purposes

--- z-to-m/webpage/script.js
+++ z-to-m/webpage/script.js
@@ -1,9 +1,17 @@
 var age = prompt("What is your age?");
 
+console.log(age)
+
 if (Number(age) < 18) {
-	alert("Sorry, you are too yound to drive this car. Powering off");
+	console.log('too young to drive');
+    alert("Sorry, you are too yound to drive this car. Powering off");
+    
 } else if (Number(age) > 18) {
-	alert("Powering On. Enjoy the ride!");
+	console.log('old enough to drive');
+    alert("Powering On. Enjoy the ride!");
+    
 } else if (Number(age) === 18) {
-	alert("Congratulations on your first year of driving. Enjoy the ride!");
+	console.log('just about old enough');
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+    
 }
