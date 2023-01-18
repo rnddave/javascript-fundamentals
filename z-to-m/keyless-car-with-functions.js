5b297879c1065ba2fbb73d4cb3b4b53290e9ad28
David Dickinson <rnddave@gmail.com>
2023-01-18 12:24:09 +0000

    making the keyless car use functions - complete

--- z-to-m/keyless-car-with-functions.js
+++ z-to-m/keyless-car-with-functions.js
@@ -21,5 +21,59 @@ if (Number(age) < 18) {
 /* checkDriverAge(92);
 it returns "Powering On. Enjoy the ride!" */
 
+//-----------------------
+
 //1. Make the above code have a function called checkDriverAge()
 
+function checkDriverAge(age) {
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+var age = prompt('how old are you?'); 
+
+checkDriverAge(age);
+
+// the above works fine for me! 
+
+//-----------------------
+
+//2. Create another function that does the same thing, called checkDriverAge2() using Function Expression.
+
+
+function checkDriverAge2(age) {
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+var age2 = checkDriverAge2(prompt('how old are you?')); 
+
+// the above works fine for me! 
+
+//-----------------------
+
+//BONUS: Instead of using the prompt. Now, only use console.log and make the checkDriverAge() function accept an argument of age,
+
+function checkDriverAge3(age) {
+    if (Number(age) < 18) {
+        console.log("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        console.log("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        console.log("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+checkDriverAge3(92);
+
+// the above works fine for me! 
