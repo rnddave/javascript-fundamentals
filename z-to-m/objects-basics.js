d75d0c138571aa5668f1866787dfb239154c4828
David Dickinson <rnddave@gmail.com>
2023-01-19 10:45:09 +0000

    basic objects

---
+++ z-to-m/objects-basics.js
@@ -0,0 +1,13 @@
+// seems to be like a dictionary or set or so in python? 
+
+var userDetails = {
+    name: 'David',
+    age: 43,
+    hobby: 'camping',
+    isMarried: true,
+    canSay: function() {
+        console.log('random stuff');
+    }
+};
+
+userDetails.canSay()
