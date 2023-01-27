d59a0b5ec5cc0b0df361307a7162f78a8d8ce457
David Dickinson <rnddave@gmail.com>
2023-01-27 09:56:55 +0000

    scope-exercises - COMPLETE

---
+++ z-to-m/aditional-control-flow.js
@@ -0,0 +1,29 @@
+// ternary operator
+
+// looks like
+
+/* 
+
+condition ? expresion1 : expression2;
+
+CONDITION ? 
+ifTrue = expression1
+else = expression2
+
+example:
+
+*/
+
+function userValid(bool) {
+    return bool;
+    
+}
+
+var answer = userValid(true) ? 'Welcome' : 'Go away';
+
+// type answer receive 'welcome'
+
+// lets add to this
+
+var autoAnswer = 'your account number is ' + ( userValid(false)  ? '88 09 79 23' : 'unavailable');
+
