87532f57c898b349751c674b8270ec73c960dd86
David Dickinson <rnddave@gmail.com>
2023-01-30 09:54:16 +0000

    working on some of the ES6 changes

---
+++ z-to-m/es6-changes.js
@@ -0,0 +1,12 @@
+// let and const
+
+// we don't use the var anymore in reality 
+
+const player = 'bobbyOrange';
+let exp = 200;
+let wizardLevel = false;
+
+if (exp > 90) {
+    // let wizardLevel = true; // this won't work as this is a local variable
+    wizardLevel = true;
+}
