35fb891c49be2639b9aef722f6d88997abae2534
David Dickinson <rnddave@gmail.com>
2023-01-30 09:20:41 +0000

    working on switch statements

--- z-to-m/aditional-control-flow.js
+++ z-to-m/aditional-control-flow.js
@@ -30,3 +30,29 @@ var answer = userValid(true) ? 'Welcome' : 'Go away';
 
 var autoAnswer = 'your account number is ' + ( userValid(false)  ? '88 09 79 23' : 'unavailable');
 
+////////////////////
+
+// SWITCH STATEMENT
+
+function moveCommand(direction) {
+    var whatHappens; 
+    switch (direction) { // kind of say 'IF direction =
+        case 'forward': // IF forward THEN
+            whatHappens = 'you found the monster';
+            break;
+        case 'back':    // IF back THEN
+            whatHappens = 'shhh the monster can hear you clattering about';
+            break;
+        case 'left':
+            whatHappens = 'can you smell the fear?';
+            break;
+        case 'right':
+            whatHappens = 'it\'s a long dark hallway';
+            break;
+        default:    // if none of these conditions match, then this is a default message
+            whatHappens = 'don\'t talk gibberish to me';
+    }
+    return whatHappens;
+
+}
+
