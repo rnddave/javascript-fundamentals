774334f1c15973a442fa84f1e754c826e6982d4c
David Dickinson <rnddave@gmail.com>
2023-02-01 11:49:55 +0000

    advanced functions - exercise - FINISH - All CORRECT - woohoo

--- z-to-m/adv-func-exercises.js
+++ z-to-m/adv-func-exercises.js
@@ -2,27 +2,32 @@
 
 //#1 Create a one line function that adds adds two parameters
 
+const oneLineFunction = (a, b) => a + b;
+
 //Closure: What does the last line return?
 const addTo = x => y => x + y
 var addToTen = addTo(10)
-addToTen(3)
+addToTen(3) // GUESS = [ 13 ] { CORRECT }
 
 //Currying: What does the last line return?
 const sum = (a, b) => a + b
 const curriedSum = (a) => (b) => a + b
-curriedSum(30)(1)
+curriedSum(30)(1) // GUESS = [ 31 ] { CORRECT }
 
 
 //Currying: What does the last line return?
 const sum = (a, b) => a + b
 const curriedSum = (a) => (b) => a + b
 const add5 = curriedSum(5)
-add5(12)
+add5(12) // GUESS = [ 17 ] { CORRECT }
 
 //Composing: What does the last line return?
 const compose = (f, g) => (a) => f(g(a));
 const add1 = (num) => num + 1;
 const add5 = (num) => num + 5;
-compose(add1, add5)(10)
+compose(add1, add5)(10) // GUESS = [ 16 ] { CORRECT }
 
 //What are the two elements of a pure function?
+
+// AVOID SIDE AFFECTS 
+// ALWAYS RETURN SOMETHING
