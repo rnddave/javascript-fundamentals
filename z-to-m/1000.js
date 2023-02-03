0d5546e0d431e50b7101b349f257c3426afa8ab8
David Dickinson <rnddave@gmail.com>
2023-02-03 17:46:18 +0000

    my 1000th contribution to github

---
+++ z-to-m/1000.js
@@ -0,0 +1,8 @@
+let myGithubContributions = 0;
+
+while (myGithubContributions < 1001) {
+    console.log(`2023-02-03 was the day I sumitted my ${myGithubContributions}th github contribution`);
+    myGithubContributions++;
+}
+
+// this was just a nothing, so that I can mark my 1000th contribution to github!
