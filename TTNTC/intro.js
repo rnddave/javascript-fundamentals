f672a3199ea6742b42a75e23e99ec360313cb8d3
David Dickinson <rnddave@gmail.com>
2023-02-04 12:44:03 +0000

    TTNTC code examples (they teach python, I am converting to JS at time of lesson)

---
+++ TTNTC/intro.js
@@ -0,0 +1,92 @@
+/*
+wow folks, there are some rude comments flying in here. This is free, no one charged you to watch this session, if you think you can use your time more effectively elsewhere, I think you can just head out, ain't no one going to hold it against you. 
+
+*/
+
+// We need a problem stement or requirements document
+
+// problem - add 2 numbers
+
+const a = 20;
+const b = 30;
+const c = a+b;
+console.log(c);
+
+// program is a sequence of instructions
+
+// but what is an assignment operator = giving something 
+//      =     etc 
+// mathematical operators 
+//      + / - *     etc
+
+// variable naming s important:: 
+
+let physicsScore = 96;
+let chemScore = 70;
+let mathScore = 84;
+const result = physicsScore + chemScore + mathScore;
+console.log(result);
+
+// the shop example again 
+
+let quantity = 10;
+let price = 24.74;
+let lineTotal = quantity * price;
+let salesTax = lineTotal*0.2;
+const total = lineTotal + salesTax;
+
+console.log(total);
+
+/* 
+
+                        PYTHON DATA TYPES
+                                |
+________________________________________________________________________
+    |               |                   |           |               |
+NUMERIC         DICTIONARY          BOOLEAN         SET         SEQUENCE
+    |                                                               |
+____________________                                   __________________________   
+|          |        |                                       |         |         |
+INT     REAL    COMPLEX                                 STRING      LIST       TUPLE
+
+
+
+NUMERIC =       integer
+                float 
+                complex number
+BOOLEAN
+DICTIONARY
+SET
+SEQUENCE =      list []
+                string
+                Tuple
+ */
+
+
+// JUST TRYING BILL EXAMPLE IN JS CONSOLE LOG
+
+let product = 'pen';
+let quantity2 = 200;
+let price2 = 1.5;
+const bill2=quantity2*price2;
+console.log('your invoice details are .......');
+console.log();
+console.log('product = ' + product);
+console.log('quantity = ' + quantity2);
+console.log('line price = ' + price);
+console.log('total = £' + bill2);
+
+// LETS DO SOME CONDITIONAL STATEMENTS
+
+function goToOffice (weather) {
+    if (weather === 'raining') {
+        console.log('don\'t forget the umbrella');
+    } else if (weather === 'snow') {
+        console.log('work from home');
+    } else {
+        console.log('enjoy the walk');
+    }
+}
+
+goToOffice('raining');
+
