ff6b96023c0b0a50a9b0bb9286fb789190d201db
David Dickinson <rnddave@gmail.com>
2023-02-07 11:31:08 +0000

    question 1 complete

--- z-to-m/advanced-loops-exercise.js
+++ z-to-m/advanced-loops-exercise.js
@@ -58,3 +58,75 @@ function checkBasket(basket, lookingFor) {
 
 }
 */
+
+const basket = ['apples', 'oranges', 'grapes'];
+const detailedBasket = {
+  apples: 5,
+  oranges: 10,
+  grapes: 1000
+}
+
+// Question #1:
+// create a function called biggestNumberInArray() that takes
+// an array as a parameter and returns the biggest number.
+// biggestNumberInArray([-1,0,3,100, 99, 2, 99]) should return 100;
+// Use at least 3 different types of javascript loops to write this:
+const array = [-1,0,3,100, 99, 2, 99] // should return 100
+const array2 = ['a', 3, 4, 2] // should return 4
+const array3 = [] // should return 0
+
+function biggestNumberInArray(arr) {
+    let bigBoy = 0;
+    for (let i=0; i < arr.length; i++) {
+        if ((arr)[i] > bigBoy) {
+            bigBoy = arr[i];
+        }
+    }
+    return bigBoy;  
+}
+// array    100
+// array2   4
+// array3   0
+
+function biggestNumberInArray2(arr) {
+    let bigBoy = 0;
+    arr.forEach(element => {
+        if ((arr)[element] > bigBoy) {
+            bigBoy = arr[element];
+        }
+    });
+    return bigBoy;
+}
+// array    100
+// array2   4
+// array3   0
+
+function biggestNumberInArray3(arr) {
+    let bigBoy = 0;
+    for (elem of arr) { 
+        if (elem > bigBoy) {
+            bigBoy = elem;
+        }
+    }; 
+    return bigBoy;
+}
+// array    100
+// array2   4
+// array3   0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
