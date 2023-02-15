f7b2797f1aba96c0f4886da6e8792bd6f1e49f30
David Dickinson <rnddave@gmail.com>
2023-02-15 11:24:11 +0000

    not much to learn for ES 2022

---
+++ z-to-m/es2022.js
@@ -0,0 +1,20 @@
+// new for 2022
+
+// .at()
+
+// useful for arrays and strings
+
+const arr = [100, 10, 50, 50000, 10000]; 
+
+// get second last item
+arr[arr.length -2]      // 50000
+
+// with at()
+
+arr.at(-2); //  50000 (little bit easier to read)
+
+const aString = 'the quick brown fox jumped over the lazy dog';
+
+aString.at(-10) // 'e'
+
+// there is something else around top level wait, but we're not at that point yet.... 
