4025eed677e14b02815869fda2a10cf6d0fb3be5
David Dickinson <rnddave@gmail.com>
2023-02-22 14:12:04 +0000

    js challenge - q3 - start

--- z-to-m/js-challenge-q1.js
+++ z-to-m/js-challenge-q1.js
@@ -107,5 +107,17 @@ doesItSum(arrForSum,2);
  1 + 1 = 2
 */
 
+// ------------ MOVING ON ------------ \\ 
+
+/*
+Question 3: Write a function that converts HEX to RGB. Then Make that function auto-dect the formats so that if you enter HEX color format it returns RGB and if you enter RGB color format it returns HEX.
+*/
+
+
+
+
+
+
+
 
 
